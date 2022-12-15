b4bdf34be268d18261a633a006c221cf2ba1b283
Takahashi-Riki <riki.riki0218@gmail.com>
2022-12-15 22:50:39 +0900

    finish making specs dry

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,5 @@ RSpec.configure do |config|
   # コントローラスペックで Devise のテストヘルパーを使用する
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include RequestSpecHelper, type: :request
+  config.include Warden::Test::Helpers
 end
